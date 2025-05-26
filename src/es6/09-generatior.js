9bd3b33868beb7871cb97d1afd2bf2aaff757812
MigueEC <filia.castillo@gmail.com>
2025-05-26 08:55:07 -0500

    Refactor generator function for improved readability and consistency

--- src/es6/09-generatior.js
+++ src/es6/09-generatior.js
@@ -1,10 +1,20 @@
-function* iterate(array){
-    for(let value of array){
-        yield value;
-    }
+function* iterate(array) {
+  for (let value of array) {
+    yield value;
+  }
 }
 
-const it = iterate(['Oscar', 'David', 'Ana', 'Juan']);
+const it = iterate(["Oscar", "David", "Ana", "Juan"]);
 console.log(it.next().value);
-console.log(it.next().value)
-console.log(it.next())
+console.log(it.next().value);
+console.log(it.next());
+
+function* getId() {
+  while (true) {
+    yield Math.random().toString(36).substring(5).toUpperCase();
+  }
+}
+
+const id = getId();
+console.log(id.next().value);
+console.log(id.next().value);
