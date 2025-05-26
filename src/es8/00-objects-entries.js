b319f5bb4f523d96f711877febc77b8ae06c6c07
MigueEC <filia.castillo@gmail.com>
2025-05-26 09:28:59 -0500

    Add implementation of countries object and log its entries

---
+++ src/es8/00-objects-entries.js
@@ -0,0 +1,3 @@
+const countries = {MX: 'Mexico', US: 'United States', CA: 'Canada'};
+const entries = Object.entries(countries);
+console.log(entries); // [['MX', 'Mexico'], ['US', 'United States'], ['CA', 'Canada']]
